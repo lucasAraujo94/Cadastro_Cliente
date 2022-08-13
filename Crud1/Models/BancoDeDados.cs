15a493cef5b817bf5757ea82f3631a38ad0ba53f
lucasAraujo94 <https://github.com/lucasAraujo94/Cadastro_Cliente>
2022-08-13 15:41:00 -0300

    Initial commit

---
+++ Crud1/Models/BancoDeDados.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud1.Models
+
+   
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Clientes>Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud1;Integrated Security=True");
+        }
+    }
+}
---
+++ Crud1/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud1.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
